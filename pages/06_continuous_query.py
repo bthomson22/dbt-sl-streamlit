a7a2e4952794e6ea0a62b161eeb724f608efa9b7
bthomson22 <brandon.thomson@dbtlabs.com>
2024-09-26 15:29:50 -0500

    Added new page

---
+++ pages/06_continuous_query.py
@@ -0,0 +1,53 @@
+# third party
+import streamlit as st
+from langchain.chat_models import init_chat_model
+from langchain.output_parsers import PydanticOutputParser
+from langchain.prompts import PromptTemplate
+from langchain.prompts.few_shot import FewShotPromptTemplate
+from langchain.schema.output_parser import OutputParserException
+from pydantic.v1.error_wrappers import ValidationError
+
+# first party
+from client import get_query_results
+from helpers import create_graphql_code, create_tabs, to_arrow_table
+from llm.examples import EXAMPLES
+from llm.prompt import EXAMPLE_PROMPT
+from llm.providers import MODELS
+from schema import Query
+
+st.set_page_config(
+    page_title="Continuous Query",
+    page_icon="🌌",
+    layout="wide",
+)
+
+st.title("ChatGPT-like clone")
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
